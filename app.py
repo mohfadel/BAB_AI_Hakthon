fc93f20922cf199e314ff59e6dc7779c75ea990c
engmohfadel <eng.mohfadel@gmail.com>
2025-02-03 11:52:56 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, redirect, url_for
+from dotenv import load_dotenv
+from ocr import get_text_read
+import os
+
+app = Flask(__name__)
+
+# Get Configuration Settings
+load_dotenv()
+ai_endpoint = os.getenv('AI_SERVICE_ENDPOINT')
+ai_key = os.getenv('AI_SERVICE_KEY')
+upload_folder = os.getenv('UPLOAD_FOLDER')
+
+# Ensure upload directory exists
+os.makedirs(upload_folder, exist_ok=True)
+
+@app.route("/", methods=["GET", "POST"])
+def upload_file():
+    if request.method == "POST":
+        file = request.files["file"]
+        if file:
+            file_path = os.path.join(upload_folder, file.filename)
+            file.save(file_path)
+            ocr_result = get_text_read(ai_endpoint, ai_key, file_path)
+            ocr_result_text = ""
+            if ocr_result.read is not None:
+                for line in ocr_result.read.blocks[0].lines:
+                    # Return the text detected in the image
+                    ocr_result_text += line.text
+                return f"OCR Text extracted from image: {file.filename} :> <br> {ocr_result_text}"
+            else:
+                return f"OCR return None Text from image file name : {file.filename}"
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ ocr.py
@@ -0,0 +1,43 @@
+import os
+import time
+
+# import namespaces
+from azure.ai.vision.imageanalysis import ImageAnalysisClient
+from azure.ai.vision.imageanalysis.models import VisualFeatures
+from azure.core.credentials import AzureKeyCredential
+    
+
+def get_text_read(ai_endpoint, ai_key, image_path):
+
+    try:
+        # Authenticate Azure AI Vision client
+        cv_client = ImageAnalysisClient(
+            endpoint=ai_endpoint,
+            credential=AzureKeyCredential(ai_key)
+        )
+
+        # Open image file
+        with open(image_path, "rb") as f:
+                image_data = f.read()
+
+        # Use Analyze image function to read text in image
+        result = cv_client.analyze(
+            image_data=image_data,
+            visual_features=[VisualFeatures.READ]
+        )
+
+        # Display the image and overlay it with the extracted text
+        if result.read is not None:
+            print("\nText:")
+
+        for line in result.read.blocks[0].lines:
+            # Return the text detected in the image
+            print(f"  {line.text}")
+        
+        return result
+                
+    except Exception as ex:
+        print(ex)
+
+if __name__ == "__main__":
+    main()
