bcc73d00c627cc81e9e9732cb932a7e2b9812205
AhmedMHkl <ahekal@nama.net.sa>
2025-02-03 12:50:11 +0200

    refactor

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from dotenv import load_dotenv
-from ocr import get_text_read
+import ocr
 import os
 
 app = Flask(__name__)
@@ -24,17 +24,21 @@ def upload_file():
         if file:
             file_path = os.path.join(upload_folder, file.filename)
             file.save(file_path)
-            ocr_result = get_text_read(ai_endpoint, ai_key, file_path)
-            ocr_result_text = "<ul>"
-            if ocr_result.read is not None:
-                for line in ocr_result.read.blocks[0].lines:
-                    # Return the text detected in the image
-                    ocr_result_text += "<li>" + line.text + "</li>"
-                ocr_result_text += "</ul>"
-                return render_template("result.html", filename=file.filename, result=ocr_result_text)
-            else:
-                return f"OCR return None Text from image file name : {file.filename}"
+            ocr_result = ocr.get_text_read(ai_endpoint, ai_key, file_path)
+            # qa_result = get_qa(ai_project_name, ai_deployment_name, ocr_result)
+            return render_result(file.filename, ocr_result, '')
     return render_template("index.html")
+
+def render_result(fileName, ocr_result, qna_result):
+    ocr_result_text = "<ul>"
+    if ocr_result.read is not None:
+        for line in ocr_result.read.blocks[0].lines:
+            # Return the text detected in the image
+            ocr_result_text += "<li>" + line.text + "</li>"
+        ocr_result_text += "</ul>"
+        return render_template("result.html", filename=fileName, result=ocr_result_text)
+    else:
+        return f"OCR return None Text from image file name : {fileName}"
 
 if __name__ == "__main__":
     app.run(debug=True)
